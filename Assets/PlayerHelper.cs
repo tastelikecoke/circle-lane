e72efb1800f4d04116484808961a0a6ded2352aa
tastelikecoke <jmerc87@gmail.com>
2022-02-15 14:06:42 +0800

    Add boss phases

--- Assets/PlayerHelper.cs
+++ Assets/PlayerHelper.cs
@@ -6,10 +6,13 @@ using Ludiq;
 
 public class PlayerHelper : MonoBehaviour
 {
+    /* This didn't work out.  But mostly because I forgot to refresh my Unit Options. */
+    /* This would work to augment bolt with C# code but I already got a bolt equivalent */
     public bool IsThereRemainingTimeLeft()
     {
         float remainingTime = Variables.Object(this).Get<float>("remainingTime");
         Variables.Object(this).Set("remainingTime", remainingTime - Time.deltaTime);
         return remainingTime - Time.deltaTime > 0f;
     }
+
 }
